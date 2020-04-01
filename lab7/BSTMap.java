c3d2bad95b21f152bc8e9b36149037656bd352e9
yunhaoo.ji <yunhaoo.ji@gmail.com>
2020-03-31 23:24:57 -0700

    doing lab7 3/31/2020

---
+++ lab7/BSTMap.java
@@ -0,0 +1,132 @@
+import java.lang.UnsupportedOperationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>,V> implements Map61B<K,V>{
+
+    private Node root;              // root of BST
+
+    private class Node {
+        private K key;              // sorted by key
+        private V val;              // associated data
+        private Node left, right;   // left and right subtrees
+        private int size;           // number of nodes in a subtree
+
+        public Node(K key, V val, int size) {
+            this.key = key;
+            this.val = val;
+            this.size = size;
+        }
+    }
+
+    /** initialize an empty symbol table */
+    public BSTMap() {
+    }
+
+    /**
+     * implement all of the methods given in Map61B
+     * except for remove, iterator and keyset
+     */
+
+    /** Removes all of the mappings from this map. */
+    @Override
+    public void clear() {
+        root.size = 0;
+        root.val = null;
+    }
+
+    /* Returns true if this map contains a mapping for the specified key. */
+    @Override
+    public boolean containsKey(K key) {
+        if (key == null) throw new IllegalArgumentException("argument to contain is null");
+        return get(key) != null;
+    }
+
+    /* Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     */
+    @Override
+    public V get(K key) {
+        if (key == null) throw new IllegalArgumentException("cannot get the a null key");
+        int cmp = key.compareTo(root.key);
+        if (cmp < 0) return get(root.left, key);
+        else if (cmp > 0) return get(root.right, key);
+        else return root.val;
+    }
+    private V get(Node x, K key) {
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else return x.val;
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    @Override
+    public int size() {
+        return size(root);
+    }
+    private int size(Node x) {
+        if (x == null) return 0;
+        else return x.size;
+    }
+
+    /* Associates the specified value with the specified key in this map.
+    * delete the specified key(and its associated value) from this symbol table
+    * if the specified value is {@code null}.
+    */
+    @Override
+    public void put(K key, V val){
+        if (key == null) throw new IllegalArgumentException("calls put() with a null key");
+        if (val == null) {
+            remove(key);    // could be remove in this case
+            return;
+        }
+        root = put(root, key, val);
+    }
+
+    private Node put(Node x, K key, V val) {
+        if (x == null) return new Node(key, val, 1);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+        x.size = 1 + size(x.left) + size(x.right); // why not x.size = 1 + x.size; 或 size(x)
+        return x;
+    }
+
+    /* Returns a Set view of the keys contained in this map. */
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Removes the mapping for the specified key from this map if present.
+     * Not required for Lab 8. If you don't implement this, throw an
+     * UnsupportedOperationException. */
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Removes the entry for the specified key only if it is currently mapped to
+     * the specified value. Not required for Lab 8. If you don't implement this,
+     * throw an UnsupportedOperationException.*/
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+//         return null;
+    }
+
+
+
+
+
+
+}
